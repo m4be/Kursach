97540806c5131cac23be158e734d8c3571825f60
m4be <danilkerch2016@yandex.ru>
2023-03-09 14:38:48 +0300

    Logger

--- Server/src/Handler.java
+++ Server/src/Handler.java
@@ -1,6 +1,6 @@
 import java.io.*;
 import java.net.Socket;
-
+import java.util.logging.Level;
 
 
 public class Handler implements Runnable {
@@ -11,7 +11,10 @@ public class Handler implements Runnable {
 
     public Handler(Socket socket) {
         clientSocket = socket;
+       // Server.LOGGER.log(Level.INFO," Request from : " + socket.getInetAddress().getHostAddress());
+
     }
+
 
     public void run() {
         try {
@@ -31,32 +34,46 @@ public class Handler implements Runnable {
             request = reader.readLine();
             System.out.println("Client: " + request);
 
+
+
             String[] str = request.split(" ");
 
             switch (str[0]) {
                 case "200": { //Авторизация
                     if(DBControl.AuthorizeUser(str[1],str[2])) //Если есть такой логин пароль
-                    {response = "201";} //То авторизация успешна
+                    {
+                        response = "201";
+                        Server.LOGGER.log(Level.INFO, "[" + clientSocket.getInetAddress().getHostAddress() + "] Authorization for [" + str[1] + "] was successful");
+                    } //То авторизация успешна
                     else{
                         response = "101";//Не успешна
+                        Server.LOGGER.log(Level.INFO,"[" + clientSocket.getInetAddress().getHostAddress() + "] Authorization for [" + str[1] + "] wasn't successful");
                     }
                     break;
                 }
                 case "300": { //Регистрация
                     if(DBControl.RegisterUser(str[1],str[2])){//Если такой учетки нет
-                    response = "301";//Регистрация успешна
+                    response = "301";
+                    Server.LOGGER.log(Level.INFO,"[" + clientSocket.getInetAddress().getHostAddress() + "] Registration for login : [" + str[1] + "] and password: ["+ str[2]+"] was successful");
+                    //Регистрация успешна
                     }
                     else{
                         response = "102";//Не уcпешна
+                        Server.LOGGER.log(Level.INFO,"[" + clientSocket.getInetAddress().getHostAddress() + "] Registration for login : [" + str[1] + "] and password: ["+ str[2]+"] wasn't successful");
+
                     }
                     break;
                 }
                 case "400": { //Админ
                     if(DBControl.AuthorizeAdmin(str[1],str[2])){//Если такой учетки нет
-                        response = "401";//Регистрация успешна
+                        response = "401";//Вход в админку успешен
+                        Server.LOGGER.log(Level.INFO,"[" + clientSocket.getInetAddress().getHostAddress() + "] Authorization for Admin: [" + str[1] + "] was successful");
+
                     }
                     else{
-                        response = "103";//Не уcпешна
+                        response = "103";//Не уcпешен
+                        Server.LOGGER.log(Level.INFO,"[" + clientSocket.getInetAddress().getHostAddress() + "] Authorization for Admin: [" + str[1] + "] wasn't successful");
+
                     }
                     break;
                 }
@@ -64,22 +81,30 @@ public class Handler implements Runnable {
                 case "500": {
                     //Пользоваетль вышел из учетки
                     response = "501";
+                    Server.LOGGER.log(Level.INFO,"[" + clientSocket.getInetAddress().getHostAddress() + "] User : [" + str[1] + "] logged out");
                     break;
                 }
                 case "800": {
                     response = "801;" + DBControl.getData(); //Отправка таблицы
+                    Server.LOGGER.log(Level.INFO,"[" + clientSocket.getInetAddress().getHostAddress() + "] Sending main table");
+
                     break;
                 }
 
                 case "900": {
                     if(str[1].equals("admin")){
                         response = "902;" + DBControl.getUserInfo(true, null);
+                        Server.LOGGER.log(Level.INFO,"[" + clientSocket.getInetAddress().getHostAddress() + "] Sending sub table for admin");
+
                     }
                     else if (str[1].equals("user")) {
                         response = "901;" + DBControl.getUserInfo(false, str[2]);
+                        Server.LOGGER.log(Level.INFO,"[" + clientSocket.getInetAddress().getHostAddress() + "] Sending sub table for user");
+
                     }
                     else{
                         response = "903";
+                        Server.LOGGER.log(Level.INFO,"[" + clientSocket.getInetAddress().getHostAddress() + "] Unable to send a sub table");
                     }
                     break;
                 }
@@ -87,29 +112,35 @@ public class Handler implements Runnable {
                 case "600":{
 
                     if(DBControl.TakeCar(str[2],str[1])) {
-                        response = "601";
+                        response = "601"; //Удалось заказать технику
+                        Server.LOGGER.log(Level.INFO,"[" + clientSocket.getInetAddress().getHostAddress() + "] User: ["+str[2] +"] successfully obtained a car: ["+ str[1]+"]");
                     }
                     else{
-                        response = "602";
+                        response = "602";//Не удалось заказать технику(Нет на складе <1)
+                        Server.LOGGER.log(Level.INFO,"[" + clientSocket.getInetAddress().getHostAddress() + "] User: ["+str[2] +"] couldn't obtain a car: ["+ str[1]+"]");
+
                     }
                     break;
                 }
                 case "700" :{
                     if(DBControl.GiveCar(str[1])) {
                         response = "701";
+                        Server.LOGGER.log(Level.INFO,"[" + clientSocket.getInetAddress().getHostAddress() + "] Car: ["+ str[1]+"] was successfully returned");
                     }
                     else{
                         response = "702";
+                        Server.LOGGER.log(Level.INFO,"Car: ["+ str[1]+"] wasn't returned");
                     }
                     break;
                 }
-
-
                 default: {
                     response = "Incorrect request from user! request was: " + request;
+                    Server.LOGGER.log(Level.WARNING, "[" + clientSocket.getInetAddress().getHostAddress() + "] " + response);
                     break;
                 }
             }
+
+
             writer.write(response);
             writer.flush();
             writer.newLine();
--- Server/src/Server.java
+++ Server/src/Server.java
@@ -1,10 +1,29 @@
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.logging.FileHandler;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import java.util.logging.SimpleFormatter;
 
 public class Server {
 
     private static ServerSocket server; // серверсокет
 
+    static Logger LOGGER = Logger.getLogger(Server.class.getName());;
+
+    static {
+        try{
+            FileHandler fh = new FileHandler("logger.txt");
+            LOGGER.addHandler(fh);
+
+            SimpleFormatter formatter = new SimpleFormatter();
+            fh.setFormatter(formatter);
+
+            LOGGER.setUseParentHandlers(false); //отключаем вывод в консоль
+        }catch (Exception e){
+            System.out.println(e.getMessage());
+        }
+    }
 
     public static void main(String[] args) {
         try {
@@ -12,6 +31,8 @@ public class Server {
                 server = new ServerSocket(8000);
                 server.setReuseAddress(true);
                 System.out.println("Server is running!");
+
+                LOGGER.log(Level.INFO," Server has started");
 
                 while (true) {
                     Socket clientSocket = server.accept();
